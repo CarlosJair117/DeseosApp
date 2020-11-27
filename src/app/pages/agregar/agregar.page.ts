131c96cfb40d67c9259cba8e844074aed4cba7a7
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2020-11-27 03:11:46 -0600

    agregar tareas

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DeseosService } from 'src/app/services/deseos.service';
+import { ListaItem } from '../models/lista-item.model';
+import { Lista } from '../models/lista.model';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarPage implements OnInit {
 
-  constructor() { }
+  lista: Lista;
+  nombreItem = '';
+
+  constructor( private deseosService: DeseosService, private route: ActivatedRoute ) {
+    const listaId = this.route.snapshot.paramMap.get('listaId');
+    this.lista = this.deseosService.obtenerLista(listaId);
+   }
 
   ngOnInit() {
+  }
+
+  agragarItem(){
+
+    if (this.nombreItem.length === 0){
+      return;
+    }
+
+    const nuevoItem = new ListaItem(this.nombreItem);
+    this.lista.items.push(nuevoItem);
+
+    this.nombreItem = '';
+    this.deseosService.guardarStorage();
   }
 
 }
